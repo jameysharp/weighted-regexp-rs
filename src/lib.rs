93484ea840fb8c404cb0a7ba55bee53d7589d5ad
Jamey Sharp <jamey@minilop.net>
2019-01-04 12:29:07 -0800

    Move all grammar combinators to new module
    Besides being nicer organizationally, this also provides a compile-time
    check that it is possible to implement new combinators outside of the
    core module.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,13 @@ extern crate quickcheck;
 
 extern crate num_traits;
 
-pub mod weights;
 pub mod core;
+pub mod grammars;
+pub mod weights;
 
 #[doc(inline)]
-pub use core::{empty, is, many, AnyRegex};
+pub use core::AnyRegex;
+#[doc(inline)]
+pub use grammars::{empty, is, many};
 #[doc(inline)]
 pub use weights::recognize::{has_match, Match};
